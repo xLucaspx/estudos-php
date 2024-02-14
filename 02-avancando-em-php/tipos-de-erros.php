2dfef999f7102009557c84804b71f22ec2e1b3ea
Lucas da Paz <xlucaspx@gmail.com>
2024-02-14 11:01:12 -0300

    comentando linhas em 02-avancando-em-php/tipos-de-erros.php

--- 02-avancando-em-php/tipos-de-erros.php
+++ 02-avancando-em-php/tipos-de-erros.php
@@ -4,14 +4,17 @@
 
 $errors = [E_NOTICE, E_WARNING, E_ERROR];
 
-// Notice é quando o PHP notifica que existe algo de errado, mas não atrapalha a execução do programa
-// por exemplo ao tentar acessar um índice inexistente em um array; neste caso, o PHP avisa e devolve nulo
-echo $errors[10];
+/* Notice é quando o PHP notifica que existe algo de errado, mas não atrapalha a execução do programa
+por exemplo ao tentar acessar um índice inexistente em um array; neste caso, o PHP avisa e devolve nulo */
 
-// Warning é quando o PHP avisa que não consegue entender determinado trecho de código, mas continua executando
-// por exemplo ao tentar converter um array para string
-echo $errors;
+// echo $errors[10];
 
-// Error é quando o PHP informa que não consegue mais executar o programa
-// por exemplo ao tentar realizar uma divisão por 0
-echo $errors[0] / 0;
+/* Warning é quando o PHP avisa que não consegue entender determinado trecho de código, mas continua executando
+por exemplo ao tentar converter um array para string */
+
+// echo $errors;
+
+/* Error é quando o PHP informa que não consegue mais executar o programa
+por exemplo ao tentar realizar uma divisão por 0 */
+
+// echo $errors[0] / 0;
