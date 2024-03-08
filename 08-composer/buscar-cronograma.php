1eecbc47644252c8e6918ed7108970cdf5d418ed
Lucas da Paz <xlucaspx@gmail.com>
2024-03-08 00:05:49 -0300

    08-composer: scripts

--- 08-composer/buscar-cronograma.php
+++ 08-composer/buscar-cronograma.php
@@ -33,7 +33,6 @@ function filtra(Crawler $crawler, string $selector): array
 
 	$lista = [];
 	for ($i = 0; $i < sizeof($res); $i += 7) {
-//		echo "{$res[$i]}, {$res[$i + 1]}, {$res[$i + 2]}, {$res[$i + 3]}, {$res[$i + 4]}, {$res[$i + 5]}, {$res[$i + 6]}" . PHP_EOL;
 		$lista[] = new Cronograma(
 			$res[$i],
 			$res[$i + 1],
@@ -53,6 +52,6 @@ $res = $client->request("GET", "https://sarc.pucrs.br/Default/Export.aspx?id=217
 
 $crawler = new Crawler($res->getBody());
 
-$cronograma = filtra($crawler, 'td>span');
+$cronograma = filtra($crawler, 'td');
 
 foreach ($cronograma as $x) echo $x . PHP_EOL;
